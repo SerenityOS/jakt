43807991163a336c4139e8b8aa75c5ee0b848adf
JT <547158+jntrnr@users.noreply.github.com>
2022-05-07 16:26:58 +1200

    Add call arg typechecking, expected errors

--- tests/main.rs
+++ tests/main.rs
@@ -17,11 +17,38 @@ fn test_samples() -> Result<(), JaktError> {
                 let mut output_path = path.clone();
                 output_path.set_extension("out");
 
-                if output_path.exists() {
-                    // We have an output to compare to, let's do it.
+                let mut error_output_path = path.clone();
+                error_output_path.set_extension("error");
 
+                if output_path.exists() || error_output_path.exists() {
+                    // We have an output to compare to, let's do it.
                     let mut compiler = Compiler::new();
-                    let cpp_string = compiler.convert_to_cpp(&path)?;
+                    let cpp_string = compiler.convert_to_cpp(&path);
+
+                    let cpp_string = match cpp_string {
+                        Ok(cpp_string) => cpp_string,
+                        Err(err) => {
+                            if error_output_path.exists() {
+                                let expected_error_msg =
+                                    std::fs::read_to_string(&error_output_path)?;
+                                let expected_error_msg = expected_error_msg.replace("\r", "");
+                                let expected_error_msg = expected_error_msg.replace("\n", "");
+
+                                let returned_error = format!("{:#?}", err);
+
+                                assert!(
+                                    returned_error.contains(&expected_error_msg),
+                                    "\r\nTest: {:?}\r\nReturned error: {}\r\nExpected error: {}",
+                                    path,
+                                    returned_error,
+                                    expected_error_msg,
+                                );
+                                continue;
+                            } else {
+                                return Err(err);
+                            }
+                        }
+                    };
 
                     let uuid = uuid::Uuid::new_v4();
 
@@ -33,7 +60,6 @@ fn test_samples() -> Result<(), JaktError> {
                         cpp_file.write_all(cpp_string.as_bytes())?;
                     }
 
-                    println!("Test: {:?}", path);
                     let pwd = std::env::current_dir()?;
 
                     #[cfg(windows)]
