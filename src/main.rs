804a8a031ed1b50765ef952e249c110cdb4de603
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-05-22 10:57:26 +0200

    main: Use pico-args for better CLI
    pico-args is a zero-dependency minimal argument parsing library that
    just handles the bare basics of argument parsing. We use it here to
    provide a basic CLI that's a little more ergonomic than before, plus
    some help output. This is mainly preparatory work for subcommands,
    specifying output files, etc. which require more sophistication than
    pico-args.
    
    The main two reasons for choosing pico-args is (1) it's super tiny and
    fast, keeping the development cycle snappy even with full builds, and
    (2) most argparsing things are handled in our own code, making it easier
    to port to jakt later on. We'd just have to replicate the low-level
    parsing facilities of pico-args, which are not too complex in comparison
    to things that structopt or clap would handle.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, process::exit};
+
+use pico_args::Arguments;
 
 use jakt::{Compiler, JaktError, Span};
 
 fn main() -> Result<(), JaktError> {
+    let arguments = parse_arguments();
+
     let mut compiler = Compiler::new();
     let mut first_error = None;
 
-    for arg in std::env::args_os().skip(1) {
-        match compiler.compile(&PathBuf::from(&arg)) {
+    for file in arguments.input_files {
+        match compiler.compile(&file) {
             Ok(_) => {}
             Err(err) => {
                 match &err {
@@ -26,6 +30,65 @@ fn main() -> Result<(), JaktError> {
     } else {
         Ok(())
     }
+}
+
+/// Make sure to keep these up-to-date if you're adding arguments.
+const USAGE: &str = "usage: jakt [-h] [FILES...]";
+
+// FIXME: Once format is stable as a const function, include USAGE in this string.
+const HELP: &str = "\
+Flags:
+  -h, --help           Print this help and exit 
+
+Options:
+  FILES...             List of files to compile. The output is `output.cpp`
+                       in the cwd.
+";
+
+#[derive(Debug)]
+struct JaktArguments {
+    input_files: Vec<PathBuf>,
+}
+
+/// Exits if the arguments are invalid or the user doesn't want to run Jakt (e.g. help output)
+fn parse_arguments() -> JaktArguments {
+    let mut pico_arguments = Arguments::from_env();
+    if pico_arguments.contains(["-h", "--help"]) {
+        println!("{}\n\n{}", USAGE, HELP);
+        exit(0);
+    }
+
+    let mut arguments = JaktArguments {
+        input_files: Vec::new(),
+    };
+
+    while let Ok(filename) = pico_arguments.free_from_str::<PathBuf>() {
+        if !filename.exists() {
+            eprintln!(
+                "jakt: error: file '{}' not found or inaccessible",
+                filename.to_string_lossy()
+            );
+            exit(1);
+        }
+        arguments.input_files.push(filename);
+    }
+
+    let extra_arguments = pico_arguments.finish();
+    if !extra_arguments.is_empty() {
+        eprintln!(
+            "jakt: error: extra arguments {}",
+            extra_arguments
+                .iter()
+                .map(|os_string| format!("'{}'", os_string.to_string_lossy()))
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    } else if arguments.input_files.is_empty() {
+        println!("{}", USAGE);
+        exit(0);
+    }
+
+    arguments
 }
 
 fn display_error(compiler: &Compiler, msg: &str, span: Span) {
