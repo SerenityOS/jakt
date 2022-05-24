SerenityOS/jakt

commit 3dc9e8f33ef87bfe0127399f0a7c6dee1e6df77a

typechecker: Support for basic definite returns
The typechecker will now detect blocks containing if statements with
literal boolean conditions as definitely returning if the respective
branch definitely returns.

src/typechecker.rs
@@ -2344,11 +2344,14 @@ fn typecheck_method(
 pub fn statement_definitely_returns(stmt: &CheckedStatement) -> bool {
     match stmt {
         CheckedStatement::Return(_) => true,
-        CheckedStatement::If(_, then_block, Some(else_stmt)) => {
-            // TODO: Things like `if true` should be also accepted as
-            //       definitely returning, if we can prove at typecheck time
-            //       that it's always truthy.
-            then_block.definitely_returns && statement_definitely_returns(else_stmt.as_ref())
+        CheckedStatement::If(CheckedExpression::Boolean(cond, _), then_block, else_stmt) => {
+            if *cond {
+                then_block.definitely_returns
+            } else if let Some(else_block) = else_stmt {
+                statement_definitely_returns(else_block)
+            } else {
+                false
+            }
         }
         CheckedStatement::Block(block) => block.definitely_returns,
         CheckedStatement::Loop(block) => block.definitely_returns,

tests/typechecker/return_if_missing_else_prong.jakt
@@ -1,5 +1,5 @@
 function foo() -> i32 {
-    if true {
+    if false {
         return 123
     }
 }

commit 4d925ef8ad5df29631ed3cdbdcdb66f7a852c0b3

typechecker: Fix definite return of nontrivial ifs
This regressed in 3dc9e8f. The test didn't catch this because it had
true as its condition.

src/typechecker.rs
@@ -2344,14 +2344,11 @@ fn typecheck_method(
 pub fn statement_definitely_returns(stmt: &CheckedStatement) -> bool {
     match stmt {
         CheckedStatement::Return(_) => true,
-        CheckedStatement::If(CheckedExpression::Boolean(cond, _), then_block, else_stmt) => {
-            if *cond {
-                then_block.definitely_returns
-            } else if let Some(else_block) = else_stmt {
-                statement_definitely_returns(else_block)
-            } else {
-                false
-            }
+        CheckedStatement::If(CheckedExpression::Boolean(true, _), then_block, _) => {
+            then_block.definitely_returns
+        }
+        CheckedStatement::If(_, then_block, Some(else_stmt)) => {
+            then_block.definitely_returns && statement_definitely_returns(else_stmt.as_ref())
         }
         CheckedStatement::Block(block) => block.definitely_returns,
         CheckedStatement::Loop(block) => block.definitely_returns,

tests/typechecker/return_if_definite_no_else_prong.jakt
@@ -1,0 +1,9 @@
+function foo() -> i32 {
+    if true {
+        return 123
+    }
+}
+
+function main() {
+    println("{}", foo())
+}

tests/typechecker/return_if_definite_no_else_prong.out
@@ -1,0 +1,1 @@
+123

tests/typechecker/return_if_prongs_nontrivial.jakt
@@ -1,0 +1,11 @@
+function max(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+function main() {
+    println("{}", max(a: 123i32, b: -123i32))
+}

tests/typechecker/return_if_prongs_nontrivial.out
@@ -1,0 +1,1 @@
+123