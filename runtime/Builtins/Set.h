619cd7e4d12bddc239bb474e22e71c218c255f8c
Mustafa Quraish <mustafaq9@gmail.com>
2022-05-26 18:07:14 -0400

    runtime: Add Set::is_empty method
    We already have this for Arrays, and it seems like a nice idea
    to generally have the same APIs where applicable across different
    containers.

--- runtime/Builtins/Set.h
+++ runtime/Builtins/Set.h
@@ -55,6 +55,7 @@ public:
     ErrorOr<AK::HashSetResult> add(T&& value) { return m_storage->table.set(move(value)); }
     ErrorOr<void> ensure_capacity(size_t capacity) { return m_storage->table.try_ensure_capacity(capacity); }
 
+    bool is_empty() const { return m_storage->table.is_empty(); }
     size_t capacity() const { return m_storage->table.capacity(); }
     size_t size() const { return m_storage->table.size(); }
     void clear() { m_storage->table.clear(); }
