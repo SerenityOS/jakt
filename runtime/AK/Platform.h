ea247e56e0c28ada656eecc4c1bda3d36d5892fa
Andreas Kling <kling@serenityos.org>
2022-05-29 23:34:53 +0200

    runtime: Disable the ALWAYS_INLINE macro
    Same motivation as 012ab174628533048a857db2d9860704d04e5a1a, but a much
    less dumb patch. Thanks Ali for pointing out the obvious :^)

--- runtime/AK/Platform.h
+++ runtime/AK/Platform.h
@@ -42,7 +42,7 @@
 #ifdef ALWAYS_INLINE
 #    undef ALWAYS_INLINE
 #endif
-#define ALWAYS_INLINE __attribute__((always_inline)) inline
+#define ALWAYS_INLINE
 
 #ifdef NEVER_INLINE
 #    undef NEVER_INLINE
