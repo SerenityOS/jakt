fabf0e86747f158b41fdd0c982f0bfc582311d1a
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-05-29 09:06:52 +0430

    tests: Replace *.{out,error,error_out} with inline expect comments
    It was getting annoying to make two files for each test, so let's
    use some new shiny inline comments instead.
    A test file may start with an expect block that specifies the stdout,
    stderr and compiler errors associated with compiling and running the
    file; previous semantics about when things are checked still apply.
    
    An expect block is a `/// Expect` comment, starting at the first line
    of the test file and spanning all the subsequent `///` comments, and
    may contain `/// -` comments expressing the output requirements as
    double-quoted string literals.
    
    For instance, the following expect block:
    
        /// Expect:
        /// - output: "Foo\n"
        /// - stderr: "Bar\n"
    
    asserts that the output is "Foo" followed by a newline, and that the
    standard error output is "Bar" followed by a newline.
    As no "error" is given, the block asserts that there are no
    compilation errors.
    
    Additionally, a file may be marked as `Expect: Skip <reason>` to make
    the test runner ignore that file.

--- tests/main.rs
+++ tests/main.rs
@@ -8,6 +8,7 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::path::PathBuf;
 use std::{env::temp_dir, ffi::OsString, fs::create_dir, io::Write, process::Command};
 
 use jakt::{Compiler, JaktError};
@@ -46,6 +47,140 @@ lazy_static! {
     };
 }
 
+fn parse_quoted_string(s: &str) -> Result<String, JaktError> {
+    let s = s.trim();
+    if !s.starts_with('"') || !s.ends_with('"') {
+        return Err(JaktError::StringError("Expected quoted string".into()));
+    }
+
+    let data = &s[1..s.len() - 1];
+    let mut result = String::new();
+    let mut last_index = 0;
+    for (i, _) in data.match_indices('\\') {
+        result.push_str(&data[last_index..i]);
+        let escaped_char = data
+            .get(i + 1..i + 2)
+            .ok_or(JaktError::StringError(format!(
+                "Expected escaped character at offset {}",
+                i
+            )))?;
+        match escaped_char {
+            "\"" => result.push('"'),
+            "\\" => result.push('\\'),
+            "a" => result.push('\x07'),
+            "b" => result.push('\x08'),
+            "f" => result.push('\x0c'),
+            "n" => result.push('\n'),
+            "r" => result.push('\r'),
+            "t" => result.push('\t'),
+            "v" => result.push('\x0b'),
+            c => {
+                return Err(JaktError::StringError(format!(
+                    "Unknown escape sequence: \\{}",
+                    c
+                )))
+            }
+        }
+        last_index = i + 2;
+    }
+
+    result.push_str(&data[last_index..]);
+
+    Ok(result)
+}
+
+fn find_results(
+    path: &PathBuf,
+) -> Result<(Option<String>, Option<String>, Option<String>), JaktError> {
+    let mut output = None;
+    let mut error = None;
+    let mut stderr = None;
+
+    if let Ok(contents) = std::fs::read_to_string(path) {
+        let contents = contents.replace("\r\n", "\n");
+        let lines = contents.lines();
+        let expect_lines = lines.take_while(|line| line.starts_with("/// "));
+
+        static SEEN_MARKER: i32 = 1;
+        static SEEN_OUTPUT: i32 = 2;
+        static SEEN_ERROR: i32 = 4;
+        static SEEN_STDERR: i32 = 8;
+
+        let mut state = 0;
+        for line in expect_lines {
+            if state & SEEN_MARKER == 0 {
+                if line.starts_with("/// Expect:") {
+                    state |= SEEN_MARKER;
+                    if line
+                        .split_once(':')
+                        .unwrap()
+                        .1
+                        .trim_start()
+                        .starts_with("Skip")
+                    {
+                        // Not a test.
+                        return Ok((None, None, None));
+                    }
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            if state & SEEN_OUTPUT == 0 && line.starts_with("/// - output: ") {
+                output = Some(parse_quoted_string(
+                    line.trim_start_matches("/// - output: "),
+                )?);
+                state |= SEEN_OUTPUT;
+                continue;
+            }
+
+            if state & SEEN_ERROR == 0 && line.starts_with("/// - error: ") {
+                error = Some(parse_quoted_string(
+                    line.trim_start_matches("/// - error: "),
+                )?);
+                state |= SEEN_ERROR;
+                continue;
+            }
+
+            if state & SEEN_STDERR == 0 && line.starts_with("/// - stderr: ") {
+                stderr = Some(parse_quoted_string(
+                    line.trim_start_matches("/// - stderr: "),
+                )?);
+                state |= SEEN_STDERR;
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    if output.is_none() && error.is_none() && stderr.is_none() {
+        let mut output_path = path.clone();
+        output_path.set_extension("out");
+
+        let mut error_output_path = path.clone();
+        error_output_path.set_extension("error");
+
+        let mut stderr_output_path = path.clone();
+        stderr_output_path.set_extension("error_out");
+
+        if output_path.exists() {
+            output = Some(std::fs::read_to_string(&output_path)?.replace("\r\n", "\n"));
+        }
+
+        if error_output_path.exists() {
+            error = Some(std::fs::read_to_string(&error_output_path)?.replace("\r\n", "\n"));
+        }
+
+        if stderr_output_path.exists() {
+            stderr = Some(std::fs::read_to_string(&stderr_output_path)?.replace("\r\n", "\n"));
+        }
+    }
+
+    Ok((output, error, stderr))
+}
+
 fn test_samples(path: &str) -> Result<(), JaktError> {
     for sample in std::fs::read_dir(path).expect("if this fails, the repo or harness is broken") {
         let sample = sample.expect("if this fails, the filesytem is having issues");
@@ -54,129 +189,124 @@ fn test_samples(path: &str) -> Result<(), JaktError> {
 
         if let Some(ext) = path.extension() {
             if ext == "jakt" {
-                // Great, we found test file
-                let mut output_path = path.clone();
-                output_path.set_extension("out");
+                // Great, we found a test file
+                let (expected_output, expected_error, expected_stderr) = find_results(&path)?;
 
-                let mut error_output_path = path.clone();
-                error_output_path.set_extension("error");
-
-                let mut stderr_output_path = path.clone();
-                stderr_output_path.set_extension("error_out");
-
-                if output_path.exists() || error_output_path.exists() || stderr_output_path.exists()
+                if expected_output.is_none()
+                    && expected_error.is_none()
+                    && expected_stderr.is_none()
                 {
-                    // We have an output to compare to, let's do it.
-                    let mut compiler = Compiler::new(Vec::new());
-                    let cpp_string = compiler.convert_to_cpp(&path);
+                    // No expectations, skip it
+                    continue;
+                }
 
-                    let cpp_string = match cpp_string {
-                        Ok(cpp_string) => {
-                            if error_output_path.exists() {
-                                let expected_error_msg =
-                                    std::fs::read_to_string(&error_output_path)?;
-                                let expected_error_msg = expected_error_msg.replace('\r', "");
-                                let expected_error_msg = expected_error_msg.replace('\n', "");
+                // We have an output to compare to, let's do it.
+                let mut compiler = Compiler::new(Vec::new());
+                let cpp_string = compiler.convert_to_cpp(&path);
 
-                                panic!(
-                                    "\r\nTest: {:?}\r\nExpected error not created: {}",
-                                    path, expected_error_msg,
-                                );
-                            }
-                            cpp_string
+                let cpp_string = match cpp_string {
+                    Ok(cpp_string) => {
+                        if expected_error.is_some() {
+                            let expected_error_msg = expected_error.unwrap();
+                            let expected_error_msg = expected_error_msg.replace('\r', "");
+                            let expected_error_msg = expected_error_msg.replace('\n', "");
+
+                            panic!(
+                                "\r\nTest: {:?}\r\nExpected error not created: {}",
+                                path, expected_error_msg,
+                            );
                         }
-                        Err(err) => {
-                            if error_output_path.exists() {
-                                let expected_error_msg =
-                                    std::fs::read_to_string(&error_output_path)?;
-                                let expected_error_msg = expected_error_msg.replace('\r', "");
-                                let expected_error_msg = expected_error_msg.replace('\n', "");
+                        cpp_string
+                    }
+                    Err(err) => {
+                        if expected_error.is_some() {
+                            let expected_error_msg = expected_error.unwrap();
+                            let expected_error_msg = expected_error_msg.replace('\r', "");
+                            let expected_error_msg = expected_error_msg.replace('\n', "");
 
-                                let returned_error = format!("{:#?}", err);
+                            let returned_error = format!("{:#?}", err);
 
-                                assert!(
-                                    returned_error.contains(&expected_error_msg),
-                                    "\r\nTest: {:?}\r\nReturned error: {}\r\nExpected error: {}",
-                                    path,
-                                    returned_error,
-                                    expected_error_msg,
-                                );
-                                continue;
-                            } else {
-                                println!("Test failed: {:?}", path);
-                                return Err(err);
-                            }
+                            assert!(
+                                returned_error.contains(&expected_error_msg),
+                                "\r\nTest: {:?}\r\nReturned error: {}\r\nExpected error: {}",
+                                path,
+                                returned_error,
+                                expected_error_msg,
+                            );
+                            continue;
+                        } else {
+                            println!("Test failed: {:?}", path);
+                            return Err(err);
                         }
-                    };
-
-                    let uuid = uuid::Uuid::new_v4();
-
-                    let mut cpp_filename = temp_dir();
-                    cpp_filename.push(format!("output{}.cpp", uuid));
-
-                    {
-                        let mut cpp_file = std::fs::File::create(&cpp_filename)?;
-                        cpp_file.write_all(cpp_string.as_bytes())?;
                     }
+                };
 
-                    let mut exe_name = temp_dir();
-                    exe_name.push(format!("output{}", uuid));
+                let uuid = uuid::Uuid::new_v4();
 
-                    let status = Command::new("clang++")
-                        .arg(&cpp_filename)
-                        .arg("-o")
-                        .arg(&exe_name)
-                        .arg("-include-pch")
-                        .arg(&*PCH_FILENAME)
-                        .args(COMMON_ARGS)
-                        .output()?;
+                let mut cpp_filename = temp_dir();
+                cpp_filename.push(format!("output{}.cpp", uuid));
 
-                    if !status.status.success() {
-                        println!("{}", String::from_utf8_lossy(&status.stdout));
-                        println!("{}", String::from_utf8_lossy(&status.stderr));
-                    }
-                    assert!(status.status.success());
+                {
+                    let mut cpp_file = std::fs::File::create(&cpp_filename)?;
+                    cpp_file.write_all(cpp_string.as_bytes())?;
+                }
 
-                    let binary_run = Command::new(&exe_name).output()?;
+                let mut exe_name = temp_dir();
+                exe_name.push(format!("output{}", uuid));
 
-                    let binary_output = String::from_utf8_lossy(&binary_run.stdout).to_string();
-                    let binary_output = binary_output.replace("\r\n", "\n");
+                let status = Command::new("clang++")
+                    .arg(&cpp_filename)
+                    .arg("-o")
+                    .arg(&exe_name)
+                    .arg("-include-pch")
+                    .arg(&*PCH_FILENAME)
+                    .args(COMMON_ARGS)
+                    .output()?;
 
-                    let binary_stderr_output =
-                        String::from_utf8_lossy(&binary_run.stderr).to_string();
-                    let binary_stderr_output = binary_stderr_output.replace("\r\n", "\n");
+                if !status.status.success() {
+                    println!("{}", String::from_utf8_lossy(&status.stdout));
+                    println!("{}", String::from_utf8_lossy(&status.stderr));
+                }
+                assert!(status.status.success());
 
-                    let baseline_text = std::fs::read_to_string(&output_path);
-                    let baseline_text = baseline_text.map(|text| text.replace("\r\n", "\n"));
+                let binary_run = Command::new(&exe_name).output()?;
 
-                    let baseline_stderr_text = std::fs::read_to_string(&stderr_output_path);
-                    let baseline_stderr_text =
-                        baseline_stderr_text.map(|text| text.replace("\r\n", "\n"));
+                let binary_output = String::from_utf8_lossy(&binary_run.stdout).to_string();
+                let binary_output = binary_output.replace("\r\n", "\n");
 
-                    let mut stderr_checked = false;
-                    if let Ok(baseline_stderr_text) = baseline_stderr_text {
-                        assert!(
-                            // Because runtime assertion text depends on file name, C++ library line names etc., the exact stderr text is both unpredictable and volatile.
-                            // Therefore, stderr checks allow to only check for some substring of the actual output,
-                            // where we can e.g. put in the exact assertion text we are checking for.
-                            // This is still vulnerable to changes in the assertion expression, but we do need to check for something.
-                            // It looks like the best solution right now.
-                            binary_stderr_output.contains(&baseline_stderr_text),
-                            "\r\n Test (stderr): {}",
-                            path.to_string_lossy()
-                        );
-                        stderr_checked = true;
-                    }
+                let binary_stderr_output = String::from_utf8_lossy(&binary_run.stderr).to_string();
+                let binary_stderr_output = binary_stderr_output.replace("\r\n", "\n");
 
-                    // If we checked stderr, we're not required to check stdout.
-                    if !stderr_checked || baseline_text.is_ok() {
-                        assert_eq!(
-                            binary_output,
-                            baseline_text?,
-                            "\r\nTest: {}",
-                            path.to_string_lossy()
-                        );
-                    }
+                let baseline_text = expected_output;
+                let baseline_text = baseline_text.map(|text| text.replace("\r\n", "\n"));
+
+                let baseline_stderr_text = expected_stderr;
+                let baseline_stderr_text =
+                    baseline_stderr_text.map(|text| text.replace("\r\n", "\n"));
+
+                let mut stderr_checked = false;
+                if let Some(baseline_stderr_text) = baseline_stderr_text {
+                    assert!(
+                        // Because runtime assertion text depends on file name, C++ library line names etc., the exact stderr text is both unpredictable and volatile.
+                        // Therefore, stderr checks allow to only check for some substring of the actual output,
+                        // where we can e.g. put in the exact assertion text we are checking for.
+                        // This is still vulnerable to changes in the assertion expression, but we do need to check for something.
+                        // It looks like the best solution right now.
+                        binary_stderr_output.contains(&baseline_stderr_text),
+                        "\r\n Test (stderr): {}",
+                        path.to_string_lossy()
+                    );
+                    stderr_checked = true;
+                }
+
+                // If we checked stderr, we're not required to check stdout.
+                if !stderr_checked && baseline_text.is_some() {
+                    assert_eq!(
+                        binary_output,
+                        baseline_text.unwrap(),
+                        "\r\nTest: {}",
+                        path.to_string_lossy()
+                    );
                 }
             }
         }
