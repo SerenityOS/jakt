SerenityOS/jakt

commit debe647c17a005d67c680e5d216c96a2a6297c08

runtime+codegen+selfhost: Make StringBuilder::append...() methods throw
In Serenity, StringBuilder has two versions of most methods: a `foo()`
which crashes on failure, and `try_foo()` which returns ErrorOr. This
commit removes `foo()` because it's unsafe, and renames `try_foo()` to
`foo()`.

The TRY() spam is real!

runtime/Jakt/Format.cpp
@@ -190,13 +190,13 @@ bool FormatParser::consume_replacement_field(size_t& index)
 ErrorOr<void> FormatBuilder::put_padding(char fill, size_t amount)
 {
     for (size_t i = 0; i < amount; ++i)
-        TRY(m_builder.try_append(fill));
+        TRY(m_builder.append(fill));
     return {};
 }
 ErrorOr<void> FormatBuilder::put_literal(StringView value)
 {
     for (size_t i = 0; i < value.length(); ++i) {
-        TRY(m_builder.try_append(value[i]));
+        TRY(m_builder.append(value[i]));
         if (value[i] == '{' || value[i] == '}')
             ++i;
     }
@@ -217,18 +217,18 @@ ErrorOr<void> FormatBuilder::put_string(
         value = value.substring_view(0, used_by_string);
 
     if (align == Align::Left || align == Align::Default) {
-        TRY(m_builder.try_append(value));
+        TRY(m_builder.append(value));
         TRY(put_padding(fill, used_by_padding));
     } else if (align == Align::Center) {
         auto const used_by_left_padding = used_by_padding / 2;
         auto const used_by_right_padding = ceil_div<size_t, size_t>(used_by_padding, 2);
 
         TRY(put_padding(fill, used_by_left_padding));
-        TRY(m_builder.try_append(value));
+        TRY(m_builder.append(value));
         TRY(put_padding(fill, used_by_right_padding));
     } else if (align == Align::Right) {
         TRY(put_padding(fill, used_by_padding));
-        TRY(m_builder.try_append(value));
+        TRY(m_builder.append(value));
     }
     return {};
 }
@@ -276,25 +276,25 @@ ErrorOr<void> FormatBuilder::put_u64(
 
     auto const put_prefix = [&]() -> ErrorOr<void> {
         if (is_negative)
-            TRY(m_builder.try_append('-'));
+            TRY(m_builder.append('-'));
         else if (sign_mode == SignMode::Always)
-            TRY(m_builder.try_append('+'));
+            TRY(m_builder.append('+'));
         else if (sign_mode == SignMode::Reserved)
-            TRY(m_builder.try_append(' '));
+            TRY(m_builder.append(' '));
 
         if (prefix) {
             if (base == 2) {
                 if (upper_case)
-                    TRY(m_builder.try_append("0B"));
+                    TRY(m_builder.append("0B"));
                 else
-                    TRY(m_builder.try_append("0b"));
+                    TRY(m_builder.append("0b"));
             } else if (base == 8) {
-                TRY(m_builder.try_append("0"));
+                TRY(m_builder.append("0"));
             } else if (base == 16) {
                 if (upper_case)
-                    TRY(m_builder.try_append("0X"));
+                    TRY(m_builder.append("0X"));
                 else
-                    TRY(m_builder.try_append("0x"));
+                    TRY(m_builder.append("0x"));
             }
         }
         return {};
@@ -302,7 +302,7 @@ ErrorOr<void> FormatBuilder::put_u64(
 
     auto const put_digits = [&]() -> ErrorOr<void> {
         for (size_t i = 0; i < used_by_digits; ++i)
-            TRY(m_builder.try_append(buffer[i]));
+            TRY(m_builder.append(buffer[i]));
         return {};
     };
 
@@ -371,16 +371,16 @@ ErrorOr<void> FormatBuilder::put_f64(
 
     if (__builtin_isnan(value) || __builtin_isinf(value)) {
         if (value < 0.0)
-            TRY(string_builder.try_append('-'));
+            TRY(string_builder.append('-'));
         else if (sign_mode == SignMode::Always)
-            TRY(string_builder.try_append('+'));
+            TRY(string_builder.append('+'));
         else if (sign_mode == SignMode::Reserved)
-            TRY(string_builder.try_append(' '));
+            TRY(string_builder.append(' '));
 
         if (__builtin_isnan(value))
-            TRY(string_builder.try_append(upper_case ? "NAN"sv : "nan"sv));
+            TRY(string_builder.append(upper_case ? "NAN"sv : "nan"sv));
         else
-            TRY(string_builder.try_append(upper_case ? "INF"sv : "inf"sv));
+            TRY(string_builder.append(upper_case ? "INF"sv : "inf"sv));
         TRY(put_string(string_builder.string_view(), align, min_width, NumericLimits<size_t>::max(), fill));
         return {};
     }
@@ -410,7 +410,7 @@ ErrorOr<void> FormatBuilder::put_f64(
         }
 
         if (zero_pad || visible_precision > 0)
-            TRY(string_builder.try_append('.'));
+            TRY(string_builder.append('.'));
 
         if (visible_precision > 0)
             TRY(format_builder.put_u64(static_cast<u64>(value), base, false, upper_case, true, Align::Right, visible_precision));
@@ -438,16 +438,16 @@ ErrorOr<void> FormatBuilder::put_f80(
 
     if (__builtin_isnan(value) || __builtin_isinf(value)) {
         if (value < 0.0l)
-            TRY(string_builder.try_append('-'));
+            TRY(string_builder.append('-'));
         else if (sign_mode == SignMode::Always)
-            TRY(string_builder.try_append('+'));
+            TRY(string_builder.append('+'));
         else if (sign_mode == SignMode::Reserved)
-            TRY(string_builder.try_append(' '));
+            TRY(string_builder.append(' '));
 
         if (__builtin_isnan(value))
-            TRY(string_builder.try_append(upper_case ? "NAN"sv : "nan"sv));
+            TRY(string_builder.append(upper_case ? "NAN"sv : "nan"sv));
         else
-            TRY(string_builder.try_append(upper_case ? "INF"sv : "inf"sv));
+            TRY(string_builder.append(upper_case ? "INF"sv : "inf"sv));
         TRY(put_string(string_builder.string_view(), align, min_width, NumericLimits<size_t>::max(), fill));
         return {};
     }
@@ -477,7 +477,7 @@ ErrorOr<void> FormatBuilder::put_f80(
         }
 
         if (visible_precision > 0) {
-            string_builder.append('.');
+            TRY(string_builder.append('.'));
             TRY(format_builder.put_u64(static_cast<u64>(value), base, false, upper_case, true, Align::Right, visible_precision));
         }
     }
@@ -494,7 +494,7 @@ ErrorOr<void> FormatBuilder::put_hexdump(ReadonlyBytes bytes, size_t width, char
         TRY(put_padding(fill, 4));
         for (size_t j = i - width; j < i; ++j) {
             auto ch = bytes[j];
-            TRY(m_builder.try_append(ch >= 32 && ch <= 127 ? ch : '.')); // silly hack
+            TRY(m_builder.append(ch >= 32 && ch <= 127 ? ch : '.')); // silly hack
         }
         return {};
     };
@@ -712,7 +712,7 @@ ErrorOr<void> Formatter<wchar_t>::format(FormatBuilder& builder, wchar_t value)
         return formatter.format(builder, static_cast<u32>(value));
     } else {
         auto codepoint = TRY(StringBuilder::create());
-        codepoint.append_code_point(value);
+        TRY(codepoint.append_code_point(value));
 
         Formatter<StringView> formatter { *this };
         return formatter.format(builder, codepoint.string_view());
@@ -791,7 +791,7 @@ void vout(FILE* file, StringView fmtstr, TypeErasedFormatParams& params, bool ne
     MUST(vformat(builder, fmtstr, params));
 
     if (newline)
-        builder.append('\n');
+        MUST(builder.append('\n'));
 
     auto const string = builder.string_view();
     auto const retval = ::fwrite(string.characters_without_null_termination(), 1, string.length(), file);
@@ -849,7 +849,7 @@ void vdbgln(StringView fmtstr, TypeErasedFormatParams& params)
 #endif
 
     MUST(vformat(builder, fmtstr, params));
-    builder.append('\n');
+    MUST(builder.append('\n'));
 
     auto const string = builder.string_view();
 

runtime/Jakt/StringBuilder.cpp
@@ -23,7 +23,7 @@ StringBuilder::StringBuilder(Array<u8> buffer)
 {
 }
 
-ErrorOr<void> StringBuilder::try_append(StringView string)
+ErrorOr<void> StringBuilder::append(StringView string)
 {
     if (string.is_empty())
         return {};
@@ -32,33 +32,17 @@ ErrorOr<void> StringBuilder::try_append(StringView string)
     return {};
 }
 
-ErrorOr<void> StringBuilder::try_append(char ch)
+ErrorOr<void> StringBuilder::append(char ch)
 {
     TRY(will_append(1));
     TRY(m_buffer.push(ch));
     return {};
 }
 
-void StringBuilder::append(StringView string)
+ErrorOr<void> StringBuilder::append(char const* characters, size_t length)
 {
-    MUST(try_append(string));
+    return append(StringView { characters, length });
 }
-
-ErrorOr<void> StringBuilder::try_append(char const* characters, size_t length)
-{
-    return try_append(StringView { characters, length });
-}
-
-void StringBuilder::append(char const* characters, size_t length)
-{
-    MUST(try_append(characters, length));
-}
-
-void StringBuilder::append(char ch)
-{
-    MUST(try_append(ch));
-}
-
 ErrorOr<String> StringBuilder::to_string() const
 {
     if (is_empty())
@@ -76,59 +60,48 @@ void StringBuilder::clear()
     static_cast<void>(m_buffer.resize(0));
 }
 
-ErrorOr<void> StringBuilder::try_append_code_point(u32 code_point)
+ErrorOr<void> StringBuilder::append_code_point(u32 code_point)
 {
-    auto nwritten = Jakt::UnicodeUtils::code_point_to_utf8(code_point, [this](char c) { append(c); });
+    ErrorOr<void> error_from_code_point = {};
+    auto nwritten = Jakt::UnicodeUtils::code_point_to_utf8(code_point, [this, &error_from_code_point](char c) {
+        auto maybe_error = append(c);
+        if (maybe_error.is_error())
+            error_from_code_point = maybe_error;
+    });
+    if (error_from_code_point.is_error())
+        return error_from_code_point.release_error();
     if (nwritten < 0) {
-        TRY(try_append(0xef));
-        TRY(try_append(0xbf));
-        TRY(try_append(0xbd));
+        TRY(append(0xef));
+        TRY(append(0xbf));
+        TRY(append(0xbd));
     }
     return {};
 }
 
-void StringBuilder::append_code_point(u32 code_point)
-{
-    MUST(try_append_code_point(code_point));
-}
-
-void StringBuilder::append_as_lowercase(char ch)
-{
-    if (ch >= 'A' && ch <= 'Z')
-        append(ch + 0x20);
-    else
-        append(ch);
-}
-
-void StringBuilder::append_escaped_for_json(StringView string)
-{
-    MUST(try_append_escaped_for_json(string));
-}
-
-ErrorOr<void> StringBuilder::try_append_escaped_for_json(StringView string)
+ErrorOr<void> StringBuilder::append_escaped_for_json(StringView string)
 {
     for (auto ch : string) {
         switch (ch) {
         case '\b':
-            TRY(try_append("\\b"));
+            TRY(append("\\b"));
             break;
         case '\n':
-            TRY(try_append("\\n"));
+            TRY(append("\\n"));
             break;
         case '\t':
-            TRY(try_append("\\t"));
+            TRY(append("\\t"));
             break;
         case '\"':
-            TRY(try_append("\\\""));
+            TRY(append("\\\""));
             break;
         case '\\':
-            TRY(try_append("\\\\"));
+            TRY(append("\\\\"));
             break;
         default:
             if (ch >= 0 && ch <= 0x1f)
-                TRY(try_appendff("\\u{:04x}", ch));
+                TRY(appendff("\\u{:04x}", ch));
             else
-                TRY(try_append(ch));
+                TRY(append(ch));
         }
     }
     return {};

runtime/Jakt/StringBuilder.h
@@ -27,32 +27,17 @@ public:
     }
     ~StringBuilder() = default;
 
-    ErrorOr<void> try_append(StringView);
-    ErrorOr<void> try_append_code_point(u32);
-    ErrorOr<void> try_append(char);
+    ErrorOr<void> append(StringView);
+    ErrorOr<void> append_code_point(u32);
+    ErrorOr<void> append(char);
     template<typename... Parameters>
-    ErrorOr<void> try_appendff(StringView&& fmtstr, Parameters const&... parameters)
+    ErrorOr<void> appendff(StringView&& fmtstr, Parameters const&... parameters)
     {
         VariadicFormatParams variadic_format_params { parameters... };
         return vformat(*this, fmtstr, variadic_format_params);
     }
-    ErrorOr<void> try_append(char const*, size_t);
-    ErrorOr<void> try_append_escaped_for_json(StringView);
-
-    void append(StringView);
-    void append(char);
-    void append_code_point(u32);
-    void append(char const*, size_t);
-
-    void append_as_lowercase(char);
-    void append_escaped_for_json(StringView);
-
-    template<typename... Parameters>
-    void appendff(StringView&& fmtstr, Parameters const&... parameters)
-    {
-        VariadicFormatParams variadic_format_params { parameters... };
-        MUST(vformat(*this, fmtstr, variadic_format_params));
-    }
+    ErrorOr<void> append(char const*, size_t);
+    ErrorOr<void> append_escaped_for_json(StringView);
 
     [[nodiscard]] ErrorOr<String> to_string() const;
 
@@ -63,7 +48,7 @@ public:
     [[nodiscard]] bool is_empty() const { return m_buffer.is_empty(); }
 
     template<class SeparatorType, class CollectionType>
-    void join(SeparatorType const& separator, CollectionType const& collection, StringView fmtstr = "{}"sv)
+    ErrorOr<void> join(SeparatorType const& separator, CollectionType const& collection, StringView fmtstr = "{}"sv)
     {
         bool first = true;
         for (auto& item : collection) {
@@ -90,13 +75,14 @@ private:
 namespace Jakt {
 
 template<typename T>
-void append_value(StringBuilder& string_builder, T const& value)
+ErrorOr<void> append_value(StringBuilder& string_builder, T const& value)
 {
     if constexpr (IsSame<String, T>)
-        string_builder.append("\"");
-    string_builder.appendff("{}", value);
+        TRY(string_builder.append("\""));
+    TRY(string_builder.appendff("{}", value));
     if constexpr (IsSame<String, T>)
-        string_builder.append("\"");
+        TRY(string_builder.append("\""));
+    return {};
 }
 
 template<typename T>
@@ -104,13 +90,13 @@ struct Formatter<JaktInternal::Array<T>> : Formatter<StringView> {
     ErrorOr<void> format(FormatBuilder& builder, JaktInternal::Array<T> const& value)
     {
         auto string_builder = TRY(StringBuilder::create());
-        string_builder.append("[");
+        TRY(string_builder.append("["));
         for (size_t i = 0; i < value.size(); ++i) {
-            append_value(string_builder, value[i]);
+            TRY(append_value(string_builder, value[i]));
             if (i != value.size() - 1)
-                string_builder.append(", ");
+                TRY(string_builder.append(", "));
         }
-        string_builder.append("]");
+        TRY(string_builder.append("]"));
         return Formatter<StringView>::format(builder, TRY(string_builder.to_string()));
     }
 };
@@ -120,15 +106,15 @@ struct Formatter<JaktInternal::Set<T>> : Formatter<StringView> {
     ErrorOr<void> format(FormatBuilder& builder, JaktInternal::Set<T> const& set)
     {
         auto string_builder = TRY(StringBuilder::create());
-        string_builder.append("{");
+        TRY(string_builder.append("{"));
         auto iter = set.iterator();
 
         for (size_t i = 0; i < set.size(); ++i) {
-            append_value(string_builder, iter.next().value());
+            TRY(append_value(string_builder, iter.next().value()));
             if (i != set.size() - 1)
-                string_builder.append(", ");
+                TRY(string_builder.append(", "));
         }
-        string_builder.append("}");
+        TRY(string_builder.append("}"));
         return Formatter<StringView>::format(builder, TRY(string_builder.to_string()));
     }
 };
@@ -138,18 +124,18 @@ struct Formatter<JaktInternal::Dictionary<K, V>> : Formatter<StringView> {
     ErrorOr<void> format(FormatBuilder& builder, JaktInternal::Dictionary<K, V> const& dict)
     {
         auto string_builder = TRY(StringBuilder::create());
-        string_builder.append("[");
+        TRY(string_builder.append("["));
         auto iter = dict.iterator();
 
         for (size_t i = 0; i < dict.size(); ++i) {
             auto item = iter.next().value();
-            append_value(string_builder, item.template get<0>());
-            string_builder.append(": ");
-            append_value(string_builder, item.template get<1>());
+            TRY(append_value(string_builder, item.template get<0>()));
+            TRY(string_builder.append(": "));
+            TRY(append_value(string_builder, item.template get<1>()));
             if (i != dict.size() - 1)
-                string_builder.append(", ");
+                TRY(string_builder.append(", "));
         }
-        string_builder.append("]");
+        TRY(string_builder.append("]"));
         return Formatter<StringView>::format(builder, TRY(string_builder.to_string()));
     }
 };
@@ -159,15 +145,28 @@ struct Formatter<Jakt::Tuple<Ts...>> : Formatter<StringView> {
     ErrorOr<void> format(FormatBuilder& builder, Jakt::Tuple<Ts...> const& tuple)
     {
         auto string_builder = TRY(StringBuilder::create());
-        string_builder.append("(");
+        TRY(string_builder.append("("));
         if constexpr (sizeof...(Ts) > 0) {
-            tuple.apply_as_args([&](auto first, auto... args) {
-                append_value(string_builder, first);
-                ((string_builder.append(", "), append_value(string_builder, args)), ...);
-            });
+            TRY(tuple.apply_as_args([&](auto first, auto... args) {
+                ErrorOr<void> append_error = {};
+
+                auto append_helper = [&](StringBuilder& string_builder, StringView value) {
+                    if (!append_error.is_error())
+                        append_error = string_builder.append(value);
+                };
+                auto append_value_helper = [&](StringBuilder& string_builder, auto const& value) {
+                    if (!append_error.is_error())
+                        append_error = append_value(string_builder, value);
+                };
+
+                append_value_helper(string_builder, first);
+                ((append_helper(string_builder, ", "), append_value_helper(string_builder, args)), ...);
+
+                return append_error;
+            }));
         }
 
-        string_builder.append(")");
+        TRY(string_builder.append(")"));
         return Formatter<StringView>::format(builder, TRY(string_builder.to_string()));
     }
 };

selfhost/lexer.jakt
@@ -7,7 +7,7 @@ import error { JaktError }
 import utility { Span }
 
 extern struct StringBuilder {
-    function append(mut this, anon s: u8)
+    function append(mut this, anon s: u8) throws
     function to_string(mut this) throws -> String
     function create() throws -> StringBuilder
 }

src/codegen.rs
@@ -1266,7 +1266,7 @@ fn codegen_enum_debug_description_getter(enum_: &CheckedEnum) -> String {
 
     output.push_str("auto builder = TRY(StringBuilder::create());");
 
-    output.push_str("this->visit(");
+    output.push_str("TRY(this->visit(");
 
     for (i, variant) in enum_.variants.iter().enumerate() {
         let name = match variant {
@@ -1275,46 +1275,55 @@ fn codegen_enum_debug_description_getter(enum_: &CheckedEnum) -> String {
             CheckedEnumVariant::WithValue(name, ..) => name,
             CheckedEnumVariant::StructLike(name, ..) => name,
         };
-        output.push_str(&format!("[&]([[maybe_unused]] {} const& that) {{\n", name));
-        output.push_str(&format!("builder.append(\"{}::{}\");", enum_.name, name));
+        output.push_str(&format!(
+            "[&]([[maybe_unused]] {} const& that) -> ErrorOr<void> {{\n",
+            name
+        ));
+        output.push_str(&format!(
+            "TRY(builder.append(\"{}::{}\"));",
+            enum_.name, name
+        ));
         match variant {
             CheckedEnumVariant::StructLike(_, fields, ..) => {
-                output.push_str("builder.append(\"(\");");
+                output.push_str("TRY(builder.append(\"(\"));");
                 for (j, field) in fields.iter().enumerate() {
-                    output.push_str(&format!("builder.append(\"{}: \");", field.name));
+                    output.push_str(&format!("TRY(builder.append(\"{}: \"));", field.name));
                     if field.type_id == crate::compiler::STRING_TYPE_ID {
-                        output.push_str("builder.append(\"\\\"\");");
+                        output.push_str("TRY(builder.append(\"\\\"\"));");
                     }
-                    output.push_str(&format!("builder.appendff(\"{{}}\", that.{});", field.name));
+                    output.push_str(&format!(
+                        "TRY(builder.appendff(\"{{}}\", that.{}));",
+                        field.name
+                    ));
                     if field.type_id == crate::compiler::STRING_TYPE_ID {
-                        output.push_str("builder.append(\"\\\"\");");
+                        output.push_str("TRY(builder.append(\"\\\"\"));");
                     }
                     if j != fields.len() - 1 {
-                        output.push_str("builder.append(\", \");");
+                        output.push_str("TRY(builder.append(\", \"));");
                     }
                 }
-                output.push_str("builder.append(\")\");");
+                output.push_str("TRY(builder.append(\")\"));");
             }
             CheckedEnumVariant::Typed(_, type_id, ..) => {
-                output.push_str("builder.append(\"(\");");
+                output.push_str("TRY(builder.append(\"(\"));");
                 if *type_id == crate::compiler::STRING_TYPE_ID {
-                    output.push_str("builder.append(\"\\\"\");");
+                    output.push_str("TRY(builder.append(\"\\\"\"));");
                 }
-                output.push_str("builder.appendff(\"{}\", that.value);");
+                output.push_str("TRY(builder.appendff(\"{}\", that.value));");
                 if *type_id == crate::compiler::STRING_TYPE_ID {
-                    output.push_str("builder.append(\"\\\"\");");
+                    output.push_str("TRY(builder.append(\"\\\"\"));");
                 }
-                output.push_str("builder.append(\")\");");
+                output.push_str("TRY(builder.append(\")\"));");
             }
             _ => {}
         }
-        output.push('}');
+        output.push_str("return {}; }");
         if i != enum_.variants.len() - 1 {
             output.push(',');
         }
     }
 
-    output.push_str(");");
+    output.push_str("));");
     output.push_str("return builder.to_string();");
     output.push_str(" }");
 
@@ -1327,12 +1336,12 @@ fn codegen_debug_description_getter(structure: &CheckedStruct, project: &Project
 
     output.push_str("auto builder = MUST(StringBuilder::create());");
 
-    output.push_str(&format!("builder.append(\"{}(\");", structure.name));
+    output.push_str(&format!("TRY(builder.append(\"{}(\"));", structure.name));
 
     for (i, field) in structure.fields.iter().enumerate() {
         output.push_str(&codegen_indent(INDENT_SIZE));
 
-        output.push_str("builder.appendff(\"");
+        output.push_str("TRY(builder.appendff(\"");
         output.push_str(&field.name);
         if field.type_id == crate::compiler::STRING_TYPE_ID {
             output.push_str(": \\\"{}\\\"");
@@ -1355,10 +1364,10 @@ fn codegen_debug_description_getter(structure: &CheckedStruct, project: &Project
         }
 
         output.push_str(&field.name);
-        output.push_str(");\n");
+        output.push_str("));\n");
     }
 
-    output.push_str("builder.append(\")\");");
+    output.push_str("TRY(builder.append(\")\"));");
     output.push_str("return builder.to_string();");
     output.push_str(" }");
 

commit 55e3b58accff5438d0b7bc0d5b651935999c737c

runtime: Wrap StringBuilder::append..() with MUST() in serenity branches
This fixes the serenity build by removing some ignore unused result
warnings. This broke after debe647c17a005d67c680e5d216c96a2a6297c08 .

runtime/Jakt/Format.cpp
@@ -823,12 +823,12 @@ void vdbgln(StringView fmtstr, TypeErasedFormatParams& params)
             ts = TimeManagement::the().monotonic_time(TimePrecision::Coarse).to_timespec();
         if (Kernel::Thread::current()) {
             auto& thread = *Kernel::Thread::current();
-            builder.appendff("{}.{:03} \033[34;1m[#{} {}({}:{})]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, Kernel::Processor::current_id(), thread.process().name(), thread.pid().value(), thread.tid().value());
+            MUST(builder.appendff("{}.{:03} \033[34;1m[#{} {}({}:{})]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, Kernel::Processor::current_id(), thread.process().name(), thread.pid().value(), thread.tid().value()));
         } else {
-            builder.appendff("{}.{:03} \033[34;1m[#{} Kernel]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, Kernel::Processor::current_id());
+            MUST(builder.appendff("{}.{:03} \033[34;1m[#{} Kernel]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, Kernel::Processor::current_id()));
         }
     } else {
-        builder.appendff("\033[34;1m[Kernel]\033[0m: ");
+        MUST(builder.appendff("\033[34;1m[Kernel]\033[0m: "));
     }
 #    else
     static TriState got_process_name = TriState::Unknown;
@@ -843,7 +843,7 @@ void vdbgln(StringView fmtstr, TypeErasedFormatParams& params)
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC_COARSE, &ts);
     if (got_process_name == TriState::True)
-        builder.appendff("{}.{:03} \033[33;1m{}({}:{})\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, process_name_buffer, getpid(), gettid());
+        MUST(builder.appendff("{}.{:03} \033[33;1m{}({}:{})\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, process_name_buffer, getpid(), gettid()));
 #    endif
 #endif
 
@@ -878,9 +878,9 @@ void vdmesgln(StringView fmtstr, TypeErasedFormatParams& params)
 
     if (Kernel::Processor::is_initialized() && Kernel::Thread::current()) {
         auto& thread = *Kernel::Thread::current();
-        builder.appendff("{}.{:03} \033[34;1m[{}({}:{})]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, thread.process().name(), thread.pid().value(), thread.tid().value());
+        MUST(builder.appendff("{}.{:03} \033[34;1m[{}({}:{})]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000, thread.process().name(), thread.pid().value(), thread.tid().value()));
     } else {
-        builder.appendff("{}.{:03} \033[34;1m[Kernel]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000);
+        MUST(builder.appendff("{}.{:03} \033[34;1m[Kernel]\033[0m: ", ts.tv_sec, ts.tv_nsec / 1000000));
     }
 #    endif
 
@@ -900,9 +900,9 @@ void v_critical_dmesgln(StringView fmtstr, TypeErasedFormatParams& params)
 #    ifdef __serenity__
     if (Kernel::Processor::is_initialized() && Kernel::Thread::current()) {
         auto& thread = *Kernel::Thread::current();
-        builder.appendff("[{}({}:{})]: ", thread.process().name(), thread.pid().value(), thread.tid().value());
+        MUST(builder.appendff("[{}({}:{})]: ", thread.process().name(), thread.pid().value(), thread.tid().value()));
     } else {
-        builder.appendff("[Kernel]: ");
+        MUST(builder.appendff("[Kernel]: "));
     }
 #    endif
 