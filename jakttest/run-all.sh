99f0271881f5e6b076648957c0748e84d7425a50
Andreas Kling <kling@serenityos.org>
2022-06-20 20:13:40 +0200

    jakttest: Add a super quick & dirty script to run all tests

---
+++ jakttest/run-all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+pass=0
+fail=0
+
+count=0
+
+for f in samples/**/*.jakt tests/**/*.jakt; do
+    count=$(expr $count + 1)
+done
+
+i=0
+
+set +m
+
+for f in samples/**/*.jakt tests/**/*.jakt; do
+    i=$(expr $i + 1)
+    $(./build/jakttest $f >/dev/null 2>/dev/null) >/dev/null 2>/dev/null && result=pass || result=fail 
+
+    echo -n "($i/$count: $pass+$fail) "
+
+    if [ "x$result" == "xpass" ]; then
+        pass=$(expr $pass + 1)
+        echo -ne "[ \033[32;1mPASS\033[0m ] "
+    else
+        fail=$(expr $fail + 1)
+        echo -ne "[ \033[31;1mFAIL\033[0m ] "
+    fi
+    echo "$f"
+
+done
+
+echo ==============================
+echo $pass passed
+echo $fail failed
+echo ==============================
