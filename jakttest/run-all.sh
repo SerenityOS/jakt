b565edda8e2908aca6bc05cf624dc65d80e980de
JT <547158+jntrnr@users.noreply.github.com>
2022-06-21 18:17:23 +1200

    jakttest: Update test harness to also show skipped tests

--- jakttest/run-all.sh
+++ jakttest/run-all.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+if [ ! -x ./build/jakttest ]; then
+    echo "jakttest binary does not exist."
+    echo 'You can build it with `cargo run jakttest/jakttest.jakt`'
+    exit 1
+fi
+
+if [ ! -x ./build/main ]; then
+    echo "selfhosted jakt binary does not exist."
+    echo 'You can build it with `cargo run selfhost/main.jakt`'
+    exit 1
+fi
+
 pass=0
 fail=0
+skipped=0
 
 count=0
 
@@ -15,13 +28,18 @@ set +m
 
 for f in samples/**/*.jakt tests/**/*.jakt; do
     i=$(expr $i + 1)
-    $(./build/jakttest $f >/dev/null 2>/dev/null) >/dev/null 2>/dev/null && result=pass || result=fail 
+    $(./build/jakttest $f >/dev/null 2>/dev/null) >/dev/null 2>/dev/null
+    
+    exitcode=$?
 
-    echo -n "($i/$count: $pass+$fail) "
+    echo -n "($i/$count: $pass+$fail+$skipped) "
 
-    if [ "x$result" == "xpass" ]; then
+    if [ $exitcode == 0 ]; then
         pass=$(expr $pass + 1)
         echo -ne "[ \033[32;1mPASS\033[0m ] "
+    elif [ $exitcode == 2 ]; then
+        skipped=$(expr $skipped + 1)
+        echo -ne "[ \033[33;1mSKIPPED\033[0m ] "
     else
         fail=$(expr $fail + 1)
         echo -ne "[ \033[31;1mFAIL\033[0m ] "
@@ -33,4 +51,5 @@ done
 echo ==============================
 echo $pass passed
 echo $fail failed
+echo $skipped skipped
 echo ==============================
