34f08203673b8f538be9b5f874380f33c12b1551
Kyle Lanmon <kyle.lanmon@gmail.com>
2022-06-21 10:31:53 -0500

    jakttest: Build binaries if they are not found
    The script would previously tell you what to do and return an error
    code. It is much nicer if it can fix the problem for you. Also, fix
    some things according to ShellCheck linter and clean up generated
    files.

--- jakttest/run-all.sh
+++ jakttest/run-all.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 if [ ! -x ./build/jakttest ]; then
-    echo "jakttest binary does not exist."
-    echo 'You can build it with `cargo run jakttest/jakttest.jakt`'
-    exit 1
+    echo "jakttest binary does not exist. Building it now."
+    cargo run jakttest/jakttest.jakt
 fi
 
 if [ ! -x ./build/main ]; then
-    echo "selfhosted jakt binary does not exist."
-    echo 'You can build it with `cargo run selfhost/main.jakt`'
-    exit 1
+    echo "selfhosted jakt binary does not exist. Building it now."
+    cargo run selfhost/main.jakt
 fi
 
 pass=0
@@ -19,7 +17,7 @@ skipped=0
 count=0
 
 for f in samples/**/*.jakt tests/**/*.jakt; do
-    count=$(expr $count + 1)
+    count=$((count + 1))
 done
 
 i=0
@@ -27,7 +25,7 @@ i=0
 set +m
 
 for f in samples/**/*.jakt tests/**/*.jakt; do
-    i=$(expr $i + 1)
+    i=$((i + 1))
     $(./build/jakttest $f >/dev/null 2>/dev/null) >/dev/null 2>/dev/null
     
     exitcode=$?
@@ -35,21 +33,23 @@ for f in samples/**/*.jakt tests/**/*.jakt; do
     echo -n "($i/$count: $pass+$fail+$skipped) "
 
     if [ $exitcode == 0 ]; then
-        pass=$(expr $pass + 1)
+        pass=$((pass + 1))
         echo -ne "[ \033[32;1mPASS\033[0m ] "
     elif [ $exitcode == 2 ]; then
-        skipped=$(expr $skipped + 1)
+        skipped=$((skipped + 1))
         echo -ne "[ \033[33;1mSKIPPED\033[0m ] "
     else
-        fail=$(expr $fail + 1)
+        fail=$((fail + 1))
         echo -ne "[ \033[31;1mFAIL\033[0m ] "
     fi
     echo "$f"
 
 done
 
+rm compiler.err compiler.out output.jakt runtest.err runtest.out
+
 echo ==============================
-echo $pass passed
-echo $fail failed
-echo $skipped skipped
+echo "$pass" passed
+echo "$fail" failed
+echo "$skipped" skipped
 echo ==============================
