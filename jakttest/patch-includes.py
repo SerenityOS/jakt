43f4f0e26b5d53b8f2027714ca69205f1a2dfa2c
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-07-10 23:44:13 +0200

    jakttest: Use a python script instead of sed
    Using sed for patching resulted to having version-dependent
    behavior in argument parsing, so I moved the patching to a
    python script supported by python ~3.6+, which should be
    more reliable.

---
+++ jakttest/patch-includes.py
@@ -0,0 +1,11 @@
+#!/bin/env python
+# ./patch.sh $in $out
+# Add `#include "../process.h"` to the output file (.. because file is under build/)
+import sys
+
+target_file = sys.argv[1]
+target_output = sys.argv[2]
+
+with open(target_file) as source, open(target_output, "w") as sink:
+    sink.write('#include "../process.h"\n')
+    sink.write(source.read())
