be60591eece8b4cb0b3832426550a350a8d8d220
Jesús Lapastora <cyber.gsuscode@gmail.com>
2022-07-14 17:44:59 +0200

    jakttest: Make Jakttest binary discover tests by itself
    Now Jakttest binary can traverse directories itself to discover tests if
    none are passed explicitly.

--- jakttest/fs.cpp
+++ jakttest/fs.cpp
@@ -1,5 +1,7 @@
 #include "fs.h"
+#include <Jakt/RefPtr.h>
 #include <Jakt/String.h>
+#include <Jakt/StringBuilder.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 
@@ -9,5 +11,51 @@ ErrorOr<void> mkdir(String path)
     if (::mkdir(path.c_string(), S_IRWXU) == -1)
         return Error::from_errno(errno);
     return ErrorOr<void> {};
+}
+
+ErrorOr<Optional<String>> DirectoryIterator::next()
+{
+    if (m_dirfd == NULL) {
+        return JaktInternal::NullOptional {};
+    }
+    auto const next_dir = readdir(m_dirfd);
+    if (next_dir == NULL) {
+        closedir(m_dirfd);
+        m_dirfd = NULL;
+        return JaktInternal::NullOptional {};
+    }
+    auto builder = TRY(StringBuilder::create());
+    TRY(builder.append_c_string(next_dir->d_name));
+    return Optional<String>(TRY(builder.to_string()));
+}
+DirectoryIterator::~DirectoryIterator()
+{
+    if (m_dirfd != NULL) {
+        closedir(m_dirfd);
+        m_dirfd = NULL;
+    }
+}
+
+ErrorOr<NonnullRefPtr<DirectoryIterator>> DirectoryIterator::from_raw_dirfd(DIR* dirfd)
+{
+    return adopt_nonnull_ref_or_enomem(new (nothrow) DirectoryIterator(dirfd));
+}
+
+ErrorOr<NonnullRefPtr<DirectoryIterator>> list_directory(String path)
+{
+    auto const dirfd = opendir(path.c_string());
+    if (dirfd == NULL) {
+        return Error::from_errno(errno);
+    }
+    return DirectoryIterator::from_raw_dirfd(dirfd);
+}
+
+ErrorOr<bool> is_directory(String path)
+{
+    struct stat st;
+    if (stat(path.c_string(), &st) < 0) {
+        return Error::from_errno(errno);
+    }
+    return S_ISDIR(st.st_mode);
 }
 }
--- jakttest/fs.h
+++ jakttest/fs.h
@@ -1,5 +1,27 @@
 #include <Jakt/Forward.h>
+#include <Jakt/String.h>
+#include <dirent.h>
 
 namespace Jakt::fs {
 ErrorOr<void> mkdir(String path);
+
+class DirectoryIterator : public RefCounted<DirectoryIterator> {
+    DIR* m_dirfd;
+    constexpr DirectoryIterator(DIR* dirfd)
+        : m_dirfd(dirfd)
+    {
+    }
+
+public:
+    static ErrorOr<NonnullRefPtr<DirectoryIterator>> from_raw_dirfd(DIR* dirfd);
+    ~DirectoryIterator();
+    constexpr DirectoryIterator(DirectoryIterator&& other)
+        : m_dirfd(other.m_dirfd)
+    {
+        other.m_dirfd = NULL;
+    }
+    ErrorOr<Optional<String>> next();
+};
+ErrorOr<NonnullRefPtr<DirectoryIterator>> list_directory(String path);
+ErrorOr<bool> is_directory(String path);
 }
