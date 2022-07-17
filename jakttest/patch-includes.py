685b8e0d27baacf92fa361eaa5f52ea8c06ad09d
Jesús Lapastora <cyber.gsuscode@gmail.com>
2022-07-17 21:25:40 +0200

    jakttest: Pre-touch output files
    For one test Jakttest might expect its output to be somewhere that the
    shell script hasn't yet gotten to. This seems to avoid Jakttest erroring
    out but it's only a temporary fix, since seems that it's not the *real*
    reason why Jakttest fails to find an output file.

--- jakttest/patch-includes.py
+++ jakttest/patch-includes.py
@@ -8,4 +8,5 @@ target_output = sys.argv[2]
 
 with open(target_file) as source, open(target_output, "w") as sink:
     sink.write('#include "../process.h"\n')
+    sink.write('#include "../fs.h"\n')
     sink.write(source.read())
