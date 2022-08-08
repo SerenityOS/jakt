ec3ed00a24b2b81bd1ca2ff69a71fde5b8b97279
Andrew Kaster <akaster@serenityos.org>
2022-08-07 19:08:09 -0600

    runtime: Refactor VERIFY() to be [[noreturn]] on all platforms

--- runtime/Jakt/Assertions.h
+++ runtime/Jakt/Assertions.h
@@ -9,9 +9,15 @@
 #if defined(KERNEL)
 #    include <Kernel/Assertions.h>
 #else
-#    include <assert.h>
-#    define VERIFY assert
-#    define VERIFY_NOT_REACHED() assert(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
+#    define _TRAP_NORETURN \
+        []() __attribute__((noreturn)) \
+        {                              \
+            __builtin_trap();          \
+            __builtin_unreachable();   \
+        }                              \
+        ()
+#    define VERIFY(expr) (__builtin_expect(!(expr), 0) ? _TRAP_NORETURN : (void)0)
+#    define VERIFY_NOT_REACHED() VERIFY(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
 namespace Jakt {
 static constexpr bool TODO = false;
 }
