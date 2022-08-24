67555e0f3c37ea0f97cc1610e9dfdeb6f00d269b
Andrew Kaster <akaster@serenityos.org>
2022-08-24 01:40:19 -0600

    runtime: Put assert() back into VERIFY() to see failure messages
    __builtin_trap() only crashes the application, it doesn't abort it
    with a nice error message that includes __FILE__ and __LINE__

--- bootstrap/stage0/runtime/Jakt/Assertions.h
+++ bootstrap/stage0/runtime/Jakt/Assertions.h
@@ -9,14 +9,16 @@
 #if defined(KERNEL)
 #    include <Kernel/Assertions.h>
 #else
-#    define _TRAP_NORETURN \
+#    include <assert.h>
+#    define _TRAP_NORETURN(expr)       \
         []() __attribute__((noreturn)) \
         {                              \
+            assert(false && #expr);    \
             __builtin_trap();          \
             __builtin_unreachable();   \
         }                              \
         ()
-#    define VERIFY(expr) (__builtin_expect(!(expr), 0) ? _TRAP_NORETURN : (void)0)
+#    define VERIFY(expr) (__builtin_expect(!(expr), 0) ? _TRAP_NORETURN(expr) : (void)0)
 #    define VERIFY_NOT_REACHED() VERIFY(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
 namespace Jakt {
 static constexpr bool TODO = false;
