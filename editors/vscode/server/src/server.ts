c32c9a1547568021887dedd9312c40e51137d76b
JT <547158+jntrnr@users.noreply.github.com>
2022-09-12 06:56:09 +1200

    vscode: Fix Windows IDE paths

--- editors/vscode/server/src/server.ts
+++ editors/vscode/server/src/server.ts
@@ -59,6 +59,7 @@ const tmpFile = tmp.fileSync();
 
 function includeFlagForPath(file_path: string): string {
     if (file_path.startsWith("file://")) {
+        file_path = decodeURI(file_path);
         return " -I " + path.dirname(fileURLToPath(file_path));
     }
     return " -I " + file_path;
