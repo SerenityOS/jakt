3924185083e13bf73377dabac7479e079754c51d
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-09-24 17:27:49 +0330

    selfhost: Build modules as separate TUs

--- cmake/jakt-executable.cmake
+++ cmake/jakt-executable.cmake
@@ -3,6 +3,10 @@
 # Note this file is used to build jakt itself and installed for jakt projects to use
 #
 
+# FIXME: Should this live in a toolchain file?
+set(JAKT_TARGET_IN "${CMAKE_SYSTEM_PROCESSOR}-unknown-${CMAKE_SYSTEM_NAME}-unknown")
+STRING(TOLOWER ${JAKT_TARGET_IN} JAKT_TARGET)
+
 function(add_jakt_compiler_flags target)
   target_compile_options("${target}" PRIVATE
     -Wno-unused-local-typedefs
@@ -43,38 +47,59 @@ function(add_jakt_compiler_flags target)
 endfunction()
 
 function(add_jakt_executable executable)
-  cmake_parse_arguments(PARSE_ARGV 1 JAKT_EXECUTABLE "" "MAIN_SOURCE;RUNTIME_DIRECTORY;COMPILER" "MODULE_SOURCES")
+  cmake_parse_arguments(PARSE_ARGV 1 JAKT_EXECUTABLE "" "MAIN_SOURCE;RUNTIME_DIRECTORY;COMPILER" "MODULE_SOURCES;INCLUDES")
   set(main_source "${CMAKE_CURRENT_LIST_DIR}/${JAKT_EXECUTABLE_MAIN_SOURCE}" )
   get_filename_component(main_base "${main_source}" NAME_WE)
-  set(cpp_output "${executable}_${main_base}.cpp")
+
+  list(APPEND cpp_files "Root Module.cpp")
+  foreach(module_source ${JAKT_EXECUTABLE_MODULE_SOURCES})
+    get_filename_component(module_base "${module_source}" NAME_WE)
+    list(APPEND cpp_files "${module_base}.cpp")
+  endforeach()
 
   if (NOT JAKT_EXECUTABLE_COMPILER)
     set(JAKT_EXECUTABLE_COMPILER Jakt::jakt)
   endif()
 
   if (NOT JAKT_EXECUTABLE_RUNTIME_DIRECTORY)
-    set(JAKT_EXECUTABLE_RUNTIME_DIRECTORY "$<TARGET_PROPERTY:${JAKT_EXECUTABLE_COMPILER},INTERFACE_INCLUDE_DIRECTORIES>")
+    set(JAKT_EXECUTABLE_COMPILER_INCLUDES "$<TARGET_PROPERTY:${JAKT_EXECUTABLE_COMPILER},INTERFACE_INCLUDE_DIRECTORIES>")
+  else()
+    set(JAKT_EXECUTABLE_COMPILER_INCLUDES "${JAKT_EXECUTABLE_RUNTIME_DIRECTORY}")
   endif()
 
-  set(binary_tmp_dir "${CMAKE_CURRENT_BINARY_DIR}/jakt_tmp")
+  list(APPEND JAKT_EXECUTABLE_COMPILER_INCLUDES ${JAKT_EXECUTABLE_INCLUDES})
+  list(PREPEND JAKT_EXECUTABLE_COMPILER_INCLUDES .)
+  list(REMOVE_DUPLICATES JAKT_EXECUTABLE_COMPILER_INCLUDES)
 
+  set(binary_tmp_dir "${CMAKE_CURRENT_BINARY_DIR}/${executable}_tmp")
+  list(TRANSFORM cpp_files PREPEND "${binary_tmp_dir}/")
+
+  file(MAKE_DIRECTORY "${binary_tmp_dir}")
 
   add_custom_command(
-    OUTPUT "${cpp_output}"
-    COMMAND "${CMAKE_COMMAND}" -E make_directory ${binary_tmp_dir}
-    COMMAND "$<TARGET_FILE:${JAKT_EXECUTABLE_COMPILER}>" -S --binary-dir "${binary_tmp_dir}" -R "${JAKT_EXECUTABLE_RUNTIME_DIRECTORY}" "${main_source}"
-    COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${CMAKE_CURRENT_BINARY_DIR}/jakt_tmp/${main_base}.cpp" "${cpp_output}"
-    COMMAND "${CMAKE_COMMAND}" -E remove "${CMAKE_CURRENT_BINARY_DIR}/jakt_tmp/${main_base}.cpp"
+    OUTPUT ${cpp_files}
+    COMMAND "$<TARGET_FILE:${JAKT_EXECUTABLE_COMPILER}>"
+      -S
+      -T "${JAKT_TARGET}"
+      --binary-dir "${binary_tmp_dir}"
+      -I "$<JOIN:${JAKT_EXECUTABLE_COMPILER_INCLUDES},;-I>"
+      "${main_source}"
     VERBATIM
     COMMENT "Building jakt file ${JAKT_EXECUTABLE_MAIN_SOURCE} with ${JAKT_EXECUTABLE_COMPILER}"
     MAIN_DEPENDENCY "${main_source}"
     DEPENDS ${JAKT_EXECUTABLE_MODULE_SOURCES}
+    COMMAND_EXPAND_LISTS
   )
-  add_custom_target("generate_${executable}" DEPENDS "${cpp_output}")
+  add_custom_target("generate_${executable}" DEPENDS ${cpp_files})
 
-  add_executable("${executable}" "${cpp_output}")
-  target_include_directories("${executable}" PRIVATE "$<BUILD_INTERFACE:${JAKT_EXECUTABLE_RUNTIME_DIRECTORY}>")
+  add_executable("${executable}")
+  foreach(file ${cpp_files})
+    target_sources("${executable}" PRIVATE "${file}")
+    set_source_files_properties("${file}" PROPERTIES GENERATED TRUE)
+  endforeach()
+  add_jakt_compiler_flags("${executable}")
+
+  target_link_libraries("${executable}" PRIVATE Jakt::jakt_main)
   target_link_libraries("${executable}" PRIVATE Jakt::jakt_runtime)
   add_dependencies("${executable}" "generate_${executable}")
-  add_jakt_compiler_flags("${executable}")
 endfunction()
