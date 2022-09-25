d0c2a6b0214561f21253db1eb2b42a687f2bb590
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-09-25 09:34:58 +0330

    meta: Add a script for updating the bootstrap compiler

---
+++ meta/update_bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+: "${CURRENT_JAKT_COMPILER:=Build/bin/jakt}"
+: "${JAKT_RUNTIME_DIR:=Build/lib}"
+
+# First, get a working build of the compiler
+"$CURRENT_JAKT_COMPILER" --runtime-library-path "$JAKT_RUNTIME_DIR" selfhost/main.jakt
+
+# Next, use that to build the selfhost
+rm -fr selfhost_build
+mkdir selfhost_build
+build/main --binary-dir selfhost_build -S -T unknown-unknown-unknown-unknown --runtime-library-path "$JAKT_RUNTIME_DIR" selfhost/main.jakt
+
+# Now copy all the things
+rm -fr bootstrap/temp
+mkdir -p bootstrap/temp
+
+cp bootstrap/stage0/README.md bootstrap/temp/
+cp -r runtime bootstrap/temp/
+cp selfhost_build/*.cpp bootstrap/temp
+cp selfhost_build/*.h bootstrap/temp
+
+# Great, now replace the old one with the new one
+rm -fr bootstrap/stage0
+mv bootstrap/temp bootstrap/stage0
+
+# Committee committee
+git add bootstrap
+git commit -m 'bootstrap: Update stage0 snapshot'
