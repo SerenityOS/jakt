dbb0fc2f2de16269786c13ba4cb0db1855ea052a
Daniel Grießhaber <griesshaber@hdm-stuttgart.de>
2022-09-30 19:01:57 +0200

    editors/vscode: Fix conversion of spans at end of file
    Previously spans that end at the last symbol of the file yould produce
    a Range object with a start of -1.

--- editors/vscode/server/src/server.ts
+++ editors/vscode/server/src/server.ts
@@ -377,7 +377,7 @@ function convertSpan(utf8_offset: number, lineBreaks: Array<number>): Position {
 
     const start_of_line_offset =
         lineBreakIndex == -1 ? 0 : lineBreaks[lineBreakIndex] + 1;
-    const character = utf8_offset - start_of_line_offset;
+    const character = Math.max(0, utf8_offset - start_of_line_offset);
 
     return { line: lineBreakIndex + 1, character };
 }
