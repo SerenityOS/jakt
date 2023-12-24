2fbe6a6133c4f500a24ad11695d0541e50925ef9
Ali Mohammad Pur <ali.mpfard@gmail.com>
2023-12-24 19:47:09 +0330

    bootstrap: Update stage0 snapshot

--- bootstrap/stage0/git.cpp
+++ bootstrap/stage0/git.cpp
@@ -3,7 +3,7 @@ namespace Jakt {
 namespace git {
 ErrorOr<ByteString> commit_hash() {
 {
-ByteString const hash = (ByteString::from_utf8_without_validation("cb8d6cab4a96155731af02f20d66dcc2e9f36d39"sv));
+ByteString const hash = (ByteString::from_utf8_without_validation("335f01dbf5a06e22152098287855aca5f98c14c9"sv));
 return hash;
 }
 }
