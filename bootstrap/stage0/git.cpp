22733d7721b72d7db84a2f3840ee6ed0c2480049
Jesús "gsus" Lapastora <cyber.gsuscode@gmail.com>
2023-12-26 19:53:27 +0100

    bootstrap: Update stage0 snapshot

--- bootstrap/stage0/git.cpp
+++ bootstrap/stage0/git.cpp
@@ -3,7 +3,7 @@ namespace Jakt {
 namespace git {
 ErrorOr<ByteString> commit_hash() {
 {
-ByteString const hash = (ByteString::from_utf8_without_validation("HEAD"sv));
+ByteString const hash = (ByteString::from_utf8_without_validation("b7a77b58846ef58182319c9b8b3aee354b14073f"sv));
 return hash;
 }
 }
